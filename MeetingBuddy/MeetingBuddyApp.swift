e402db0f925d277156eb4b6ab29a7811c10ce83d
connie xu <conniecodes1@gmail.com>
2026-05-21 23:04:44 -0400

    Initial Commit

---
+++ MeetingBuddy/MeetingBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingBuddyApp.swift
+//  MeetingBuddy
+//
+//  Created by connie on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct MeetingBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
